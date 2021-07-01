8e52f9608472317fbc0bfc25f3fe7798a4815d71
Zac <zacharyflynn06@gmail.com>
2021-07-01 13:59:16 -0600

    add README contents

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,10 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
+    @user = User.find_or_create_by(username: params[:username])
+    
+    if @user
+      render json: UserSerializer.new(@user), status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
